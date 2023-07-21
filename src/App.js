5460e5041da217bd7bea424fdd5f44e06a6b8193
Ivan Bezniak <ivan.bezniak@gmail.com>
2023-07-21 21:21:02 +0300

    Application for sending invitations to users. Hooks are used as well as user search from the array list.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import './index.scss';
+import {Success} from './components/Success';
+import {Users} from './components/Users/Users';
+
+// Here is the list of users: https://reqres.in/api/users
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [users, setUsers] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [searchValue, setSearchValue] = useState('');
+    const [invites, setInvites] = useState([]);
+    const [success, setSuccess] = useState(false);
+
+
+    useEffect(() => {
+        fetch('https://reqres.in/api/users')
+            .then((res) => res.json())
+            .then((json) => {
+                setUsers(json.data);
+            }).catch(err => {
+            console.log(err);
+            alert('Error while getting users');
+        }).finally(() => setIsLoading(false))
+    }, [])
+
+    const onChangeSearchValue = (event) => {
+        setSearchValue(event.target.value);
+    }
+
+
+    const onClickInvite = (id) => {
+        if (invites.includes(id)) {
+            setInvites(prev => prev.filter(_id => _id !== id));
+        } else {
+            setInvites(prev => [...prev, id]);
+        }
+    }
+
+    const onClickSendInvites = () => {
+        setSuccess(true);
+    }
+
+    return (
+        <div className="App">
+
+            {
+                success
+                    ? (<Success count={invites.length}/>)
+                    : (<Users searchValue={searchValue}
+                              items={users}
+                              isLoading={isLoading}
+                              onChangeSearchValue={onChangeSearchValue}
+                              onClickInvite={onClickInvite}
+                              invites={invites}
+                              onClickSendInvites={onClickSendInvites}
+                        />
+                    )
+            }
+        </div>
+    );
 }
 
 export default App;
